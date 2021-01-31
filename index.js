b6063fdf500ddce9fbc56520399defc46a005074
Kerron Donawa <kerrondonawa@gmail.com>
2021-01-31 13:03:40 -0400

    Setup web server with express

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("test");
+const express = require('express');
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email">
+                <input name="password" placeholder="password">
+                <input name="passwordConfirmation" placeholder="password confirmation">
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `);
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('Account Created');
+})
+
+
+app.listen(3000, () => {
+    console.log('listening');
+});
