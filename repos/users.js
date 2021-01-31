cfb55105934524ba9e5b0d97c40a13cf6cdf4d70
Kerron Donawa <kerrondonawa@gmail.com>
2021-01-31 17:33:50 -0400

    Added an update method

--- repos/users.js
+++ repos/users.js
@@ -15,25 +15,34 @@ class UsersRepository{
         }
     }
 
-    async getAll() {
+    async findAll() {
         return JSON.parse(
             await fs.promises.readFile(this.filename)
         );
     }
-    async getById(id){
-        const records = await this.getAll();
+    async findById(id){
+        const records = await this.findAll();
         return records.find(data => data.id === id);
     }
     async insert(data) {
-        const records = await this.getAll();
+        const records = await this.findAll();
         records.push({id: this.randomId(), ...data});
         await this.writeAll(records);
         
     }
-    async deleteById(id){
-        const records = await this.getAll();
+    async delete(id){
+        const records = await this.findAll();
         const updated = records.filter(data => data.id !== id);
         await this.writeAll(updated);
+    }
+    async update(id, update){
+        const records = await this.findAll();
+        const record = records.find(data => data.id === id); 
+        if(!record){
+            throw new Error(`There is no record with the id: ${id}`);
+        }
+        Object.assign(record, update);
+        await this.writeAll(records);
     }
     async writeAll(records) {
         await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
@@ -46,8 +55,8 @@ class UsersRepository{
 async function test(){
     const userRepo = new UsersRepository('users.json');
     //await userRepo.insert({email:'test@email.com', password:'password'});
-    //const users = await userRepo.getAll();
-    const user = await userRepo.deleteById('98ba24f0');
+    //const users = await userRepo.findAll();
+    await userRepo.update('e21ff5b7', {password: 'new-password'});
     //console.log(user);
 }
 
