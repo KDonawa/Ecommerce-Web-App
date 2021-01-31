a7a58d80235a060f7aab7c811e38cfdcab2f9246
Kerron Donawa <kerrondonawa@gmail.com>
2021-01-31 16:06:23 -0400

    added get and insert methods to database

--- repos/users.js
+++ repos/users.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const fs = require('fs');
 
 class UsersRepository{
@@ -12,6 +13,24 @@ class UsersRepository{
             fs.writeFileSync(this.filename, '[]');
         }
     }
+
+    async getAll() {
+        return JSON.parse(
+            await fs.promises.readFile(this.filename)
+        );
+    }
+    async insert(data) {
+        const records = await this.getAll();
+        records.push(data);
+        await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    }
 }
 
-const userRepo = new UsersRepository('users.json');
+async function test(){
+    const userRepo = new UsersRepository('users.json');
+    userRepo.insert({email:'test@email.com', password:'password'});
+    const users = await userRepo.getAll();
+    console.log(users);
+}
+
+test();
