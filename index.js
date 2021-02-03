1d83efaaef9710e64450183195264db2bbd4bf2f
Kerron Donawa <kerrondonawa@gmail.com>
2021-02-03 18:55:23 -0400

    Improved look of sign in/sign up page

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
 
 const app = express();
+
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieSession({keys: ['fj3fzn74jdtebx93ydb2']}));
 app.use(authRouter);
