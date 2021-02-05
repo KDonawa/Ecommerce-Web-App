e11d49792be4993ba0b132a319fd3e2dda3b2b89
Kerron Donawa <kerrondonawa@gmail.com>
2021-02-05 13:19:28 -0400

    Added products page

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const multer = require('multer');
 
-const {handleValidationErrors} = require('./middlewares');
+const { handleValidationErrors } = require('./middlewares');
 const productsRepo = require('../../repos/products');
 const newProductTemplate = require('../../views/admin/products/new');
+const productsIndexTemplate = require('../../views/admin/products/index');
 const { validateNewProductTitle, validateNewProductPrice,
 } = require('./validators');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', (req, res) => {
-
+router.get('/admin/products', async (req, res) => {
+    const products = await productsRepo.find({});
+    res.send(productsIndexTemplate({ products }));
 });
 
 router.get('/admin/products/new', (req, res) => {
@@ -26,9 +28,10 @@ router.post('/admin/products/new', upload.single('image'),
     handleValidationErrors(newProductTemplate),
     async (req, res) => {
         const image = req.file.buffer.toString('base64');
-        const {title, price} = req.body;
-        await productsRepo.insert({title, price, image});
-        res.send('product added');
+        const { title, price } = req.body;
+        await productsRepo.insert({ title, price, image });
+        
+        res.redirect('/admin/products');
     }
 );
 
